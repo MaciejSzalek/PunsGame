b631857fbb92fb98570ec5bba032f1c3126d8b09
MaciejSzalek <maciejszalek84@gmail.com>
2018-11-13 16:58:14 +0100

    commit

--- app/src/main/java/com/puns/punsgame/Game.java
+++ app/src/main/java/com/puns/punsgame/Game.java
@@ -1,24 +1,19 @@
 package com.puns.punsgame;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
-
-    Button gameButton;
-    Button setTimerButton;
+public class Game extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        gameButton = findViewById(R.id.game_button);
-        setTimerButton = findViewById(R.id.set_timer_button);
+        setContentView(R.layout.activity_game);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitleTextColor(getResources().getColor(R.color.text_color));
@@ -29,17 +24,5 @@ public class MainActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
-
-        gameButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                goToGameActivity();
-            }
-        });
-
-    }
-    public void goToGameActivity(){
-        Intent intent = new Intent(MainActivity.this, Game.class);
-        startActivity(intent);
     }
 }
