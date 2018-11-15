7978e1fbafc9ffd08b8028842ef0b5bb22ef66ef
MaciejSzalek <maciejszalek84@gmail.com>
2018-11-15 16:12:36 +0100

    commit

--- app/src/main/java/com/puns/punsgame/EditorActivity.java
+++ app/src/main/java/com/puns/punsgame/EditorActivity.java
@@ -1,15 +1,98 @@
 package com.puns.punsgame;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class EditorActivity extends AppCompatActivity {
+
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listCategory;
+    HashMap<String, List<String>> listPun;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(getResources().getColor(R.color.text_color));
+        toolbar.setTitle(R.string.puns_game);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        //get list view
+        expListView = findViewById(R.id.exp_list);
+        //set category and puns
+        setPunsList();
+        listAdapter = new ExpandableListAdapter(this, listCategory, listPun);
+        expListView.setAdapter(listAdapter);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.add_pun:
+                Toast.makeText(EditorActivity.this, "add pun", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.delete_pun:
+                Toast.makeText(EditorActivity.this, "delete pun", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    private void setPunsList(){
+        listCategory = new ArrayList<>();
+        listPun = new HashMap<>();
+
+        listCategory.add("cat 1");
+        listCategory.add("cat 2");
+        listCategory.add("cat 3");
+
+        List<String> child_1 = new ArrayList<>();
+        child_1.add("cat 1: child 1");
+        child_1.add("cat 1: child 2");
+        child_1.add("cat 1: child 3");
+
+        List<String> child_2 = new ArrayList<>();
+        child_2.add("cat 2: child 1");
+        child_2.add("cat 2: child 2");
+        child_2.add("cat 2: child 3");
+
+        List<String> child_3 = new ArrayList<>();
+        child_3.add("cat 3: child 1");
+        child_3.add("cat 3: child 2");
+        child_3.add("cat 3: child 3");
+
+        listPun.put(listCategory.get(0), child_1);
+        listPun.put(listCategory.get(1), child_2);
+        listPun.put(listCategory.get(2), child_3);
+    }
+    private void importExelToSQLite(Context context){
+
     }
 }
---
+++ app/src/main/java/com/puns/punsgame/ExpandableListAdapter.java
@@ -0,0 +1,101 @@
+package com.puns.punsgame;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by Maci on 2018-05-15.
+ */
+
+public class ExpandableListAdapter extends BaseExpandableListAdapter{
+
+    private Context _context;
+    private List<String> _listCategory;
+    private HashMap<String, List<String>> _listPun;
+
+    public ExpandableListAdapter(Context context , List<String> listCategory,
+                                 HashMap<String, List<String>> listPun){
+        this._context = context;
+        this._listCategory = listCategory;
+        this._listPun = listPun;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return this._listCategory.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return this._listPun.get(this._listCategory.get(groupPosition)).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return this._listCategory.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return this._listPun.get(this._listCategory.get(groupPosition))
+                .get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+
+        String headerTitle = (String) getGroup(groupPosition);
+        if(convertView == null){
+            LayoutInflater inflater = (LayoutInflater) this._context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.category_list, null);
+        }
+        TextView listHeader = convertView.findViewById(R.id.list_category_txt);
+        listHeader.setTypeface(null, Typeface.BOLD);
+        listHeader.setText(headerTitle);
+
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition,
+                             boolean isLastChild, View convertView, ViewGroup parent) {
+        final String childText = (String) getChild(groupPosition, childPosition);
+        if(convertView == null){
+            LayoutInflater inflater = (LayoutInflater) this._context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.pun_list, null);
+        }
+        TextView txtListChild = convertView.findViewById(R.id.list_child_txt);
+        txtListChild.setText(childText);
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+}
